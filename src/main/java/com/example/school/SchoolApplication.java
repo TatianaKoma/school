18eb6d1439399491ccdee13dc8ddfa10e7e7248b
Tatiana Komarova <tatiana78977ua@gmail.com>
2022-12-17 23:40:52 +0200

    task-2: Builder was added

--- src/main/java/com/example/school/SchoolApplication.java
+++ src/main/java/com/example/school/SchoolApplication.java
@@ -8,8 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 
-import java.util.List;
-import java.util.UUID;
+import static java.util.UUID.randomUUID;
 
 @SpringBootApplication
 public class SchoolApplication {
@@ -17,30 +16,83 @@ public class SchoolApplication {
     public static void main(String[] args) {
         SpringApplication.run(SchoolApplication.class, args);
 
-        var group1A = new Group(UUID.randomUUID(), "1A", new ArrayList<>());
-        var group1B = new Group(UUID.randomUUID(), "1B", new ArrayList<>());
-        var group2A = new Group(UUID.randomUUID(), "2A", new ArrayList<>());
-
-        var student1 = new Student(UUID.randomUUID(), "Igor", "Petrov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1A);
-        var student2 = new Student(UUID.randomUUID(), "Oleg", "Ivanov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1A);
-        var student3 = new Student(UUID.randomUUID(), "Maxim", "Svetlov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1A);
-        List<Student> students1A = new ArrayList<>();
+        var group1A = new Group(randomUUID(), "1A", new ArrayList<>());
+        var group1B = new Group(randomUUID(), "1B", new ArrayList<>());
+        var group2A = new Group(randomUUID(), "2A", new ArrayList<>());
+        var student1 = Student.builder()
+                .id(randomUUID())
+                .name("Igor")
+                .surname("Petrov")
+                .startDate(OffsetDateTime.parse("2022-09-01T09:00:00+01:00"))
+                .group(group1A)
+                .build();
+        var student2 = Student.builder()
+                .id(randomUUID())
+                .name("Oleg")
+                .surname("Ivanov")
+                .startDate(OffsetDateTime.parse("2022-09-01T09:00:00+01:00"))
+                .group(group1A)
+                .build();
+        var student3 = Student.builder()
+                .id(randomUUID())
+                .name("Maxim")
+                .surname("Svetlov")
+                .startDate(OffsetDateTime.parse("2022-09-01T09:00:00+01:00"))
+                .group(group1A)
+                .build();
+        var students1A = new ArrayList<Student>();
         students1A.add(student1);
         students1A.add(student2);
         students1A.add(student3);
 
-        var student4 = new Student(UUID.randomUUID(), "Irina", "Kasanova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1B);
-        var student5 = new Student(UUID.randomUUID(), "Svetlana ", "Limova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1B);
-        var student6 = new Student(UUID.randomUUID(), "Maria", "Belova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1B);
-        List<Student> students1B = new ArrayList<>();
+        var student4 = Student.builder()
+                .id(randomUUID())
+                .name("Irina")
+                .surname("Kasanova")
+                .startDate(OffsetDateTime.parse("2022-09-01T09:00:00+01:00"))
+                .group(group1B)
+                .build();
+        var student5 = Student.builder()
+                .id(randomUUID())
+                .name("Svetlana")
+                .surname("Limova")
+                .startDate(OffsetDateTime.parse("2022-09-01T09:00:00+01:00"))
+                .group(group1B)
+                .build();
+        var student6 = Student.builder()
+                .id(randomUUID())
+                .name("Maria")
+                .surname("Belova")
+                .startDate(OffsetDateTime.parse("2022-09-01T09:00:00+01:00"))
+                .group(group1B)
+                .build();
+        var students1B = new ArrayList<Student>();
         students1B.add(student4);
         students1B.add(student5);
         students1B.add(student6);
 
-        var student7 = new Student(UUID.randomUUID(), "Elena", "Kolesova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group2A);
-        var student8 = new Student(UUID.randomUUID(), "Olexandr", "Litvinov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group2A);
-        var student9 = new Student(UUID.randomUUID(), "Sophia", "Minina", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group2A);
-        List<Student> students2A = new ArrayList<>();
+        var student7 = Student.builder()
+                .id(randomUUID())
+                .name("Elena")
+                .surname("Kolesova")
+                .startDate(OffsetDateTime.parse("2022-09-01T09:00:00+01:00"))
+                .group(group2A)
+                .build();
+        var student8 = Student.builder()
+                .id(randomUUID())
+                .name("Olexandr")
+                .surname("Litvinov")
+                .startDate(OffsetDateTime.parse("2022-09-01T09:00:00+01:00"))
+                .group(group2A)
+                .build();
+        var student9 = Student.builder()
+                .id(randomUUID())
+                .name("Sophia")
+                .surname("Minina")
+                .startDate(OffsetDateTime.parse("2022-09-01T09:00:00+01:00"))
+                .group(group2A)
+                .build();
+        var students2A = new ArrayList<Student>();
         students2A.add(student7);
         students2A.add(student8);
         students2A.add(student9);
