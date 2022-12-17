7819c48d73a76876a05886d0be3bc508a17b37ba
Tatiana Komarova <tatiana78977ua@gmail.com>
2022-12-17 22:20:14 +0200

    task-2: UUID generator was deleted

--- src/main/java/com/example/school/SchoolApplication.java
+++ src/main/java/com/example/school/SchoolApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @SpringBootApplication
 public class SchoolApplication {
@@ -15,29 +16,29 @@ public class SchoolApplication {
     public static void main(String[] args) {
         SpringApplication.run(SchoolApplication.class, args);
 
-        Group group1A = new Group(UUIDGenerator.generateType4UUID(), "1A", new ArrayList<>());
-        Group group1B = new Group(UUIDGenerator.generateType4UUID(), "1B", new ArrayList<>());
-        Group group2A = new Group(UUIDGenerator.generateType4UUID(), "2A", new ArrayList<>());
+        Group group1A = new Group(UUID.randomUUID(), "1A", new ArrayList<>());
+        Group group1B = new Group(UUID.randomUUID(), "1B", new ArrayList<>());
+        Group group2A = new Group(UUID.randomUUID(), "2A", new ArrayList<>());
 
-        Student student1 = new Student(UUIDGenerator.generateType4UUID(), "Igor", "Petrov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1A);
-        Student student2 = new Student(UUIDGenerator.generateType4UUID(), "Oleg", "Ivanov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1A);
-        Student student3 = new Student(UUIDGenerator.generateType4UUID(), "Maxim", "Svetlov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1A);
+        Student student1 = new Student(UUID.randomUUID(), "Igor", "Petrov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1A);
+        Student student2 = new Student(UUID.randomUUID(), "Oleg", "Ivanov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1A);
+        Student student3 = new Student(UUID.randomUUID(), "Maxim", "Svetlov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1A);
         List<Student> students1A = new ArrayList<>();
         students1A.add(student1);
         students1A.add(student2);
         students1A.add(student3);
 
-        Student student4 = new Student(UUIDGenerator.generateType4UUID(), "Irina", "Kasanova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1B);
-        Student student5 = new Student(UUIDGenerator.generateType4UUID(), "Svetlana ", "Limova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1B);
-        Student student6 = new Student(UUIDGenerator.generateType4UUID(), "Maria", "Belova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1B);
+        Student student4 = new Student(UUID.randomUUID(), "Irina", "Kasanova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1B);
+        Student student5 = new Student(UUID.randomUUID(), "Svetlana ", "Limova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1B);
+        Student student6 = new Student(UUID.randomUUID(), "Maria", "Belova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1B);
         List<Student> students1B = new ArrayList<>();
         students1B.add(student4);
         students1B.add(student5);
         students1B.add(student6);
 
-        Student student7 = new Student(UUIDGenerator.generateType4UUID(), "Elena", "Kolesova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group2A);
-        Student student8 = new Student(UUIDGenerator.generateType4UUID(), "Olexandr", "Litvinov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group2A);
-        Student student9 = new Student(UUIDGenerator.generateType4UUID(), "Sophia", "Minina", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group2A);
+        Student student7 = new Student(UUID.randomUUID(), "Elena", "Kolesova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group2A);
+        Student student8 = new Student(UUID.randomUUID(), "Olexandr", "Litvinov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group2A);
+        Student student9 = new Student(UUID.randomUUID(), "Sophia", "Minina", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group2A);
         List<Student> students2A = new ArrayList<>();
         students2A.add(student7);
         students2A.add(student8);
--- src/main/java/com/example/school/UUIDGenerator.java
+++
@@ -1,9 +0,0 @@
-package com.example.school;
-
-import java.util.UUID;
-
-public final class UUIDGenerator {
-    public static UUID generateType4UUID() {
-        return UUID.randomUUID();
-    }
-}
