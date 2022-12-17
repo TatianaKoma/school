456b75d5ca18e686f603ee4499a0a34bdd19cf24
Tatiana Komarova <tatiana78977ua@gmail.com>
2022-12-17 22:24:32 +0200

    task-2: var was added

--- src/main/java/com/example/school/SchoolApplication.java
+++ src/main/java/com/example/school/SchoolApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -16,29 +17,29 @@ public class SchoolApplication {
     public static void main(String[] args) {
         SpringApplication.run(SchoolApplication.class, args);
 
-        Group group1A = new Group(UUID.randomUUID(), "1A", new ArrayList<>());
-        Group group1B = new Group(UUID.randomUUID(), "1B", new ArrayList<>());
-        Group group2A = new Group(UUID.randomUUID(), "2A", new ArrayList<>());
+        var group1A = new Group(UUID.randomUUID(), "1A", new ArrayList<>());
+        var group1B = new Group(UUID.randomUUID(), "1B", new ArrayList<>());
+        var group2A = new Group(UUID.randomUUID(), "2A", new ArrayList<>());
 
-        Student student1 = new Student(UUID.randomUUID(), "Igor", "Petrov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1A);
-        Student student2 = new Student(UUID.randomUUID(), "Oleg", "Ivanov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1A);
-        Student student3 = new Student(UUID.randomUUID(), "Maxim", "Svetlov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1A);
+        var student1 = new Student(UUID.randomUUID(), "Igor", "Petrov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1A);
+        var student2 = new Student(UUID.randomUUID(), "Oleg", "Ivanov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1A);
+        var student3 = new Student(UUID.randomUUID(), "Maxim", "Svetlov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1A);
         List<Student> students1A = new ArrayList<>();
         students1A.add(student1);
         students1A.add(student2);
         students1A.add(student3);
 
-        Student student4 = new Student(UUID.randomUUID(), "Irina", "Kasanova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1B);
-        Student student5 = new Student(UUID.randomUUID(), "Svetlana ", "Limova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1B);
-        Student student6 = new Student(UUID.randomUUID(), "Maria", "Belova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1B);
+        var student4 = new Student(UUID.randomUUID(), "Irina", "Kasanova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1B);
+        var student5 = new Student(UUID.randomUUID(), "Svetlana ", "Limova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1B);
+        var student6 = new Student(UUID.randomUUID(), "Maria", "Belova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group1B);
         List<Student> students1B = new ArrayList<>();
         students1B.add(student4);
         students1B.add(student5);
         students1B.add(student6);
 
-        Student student7 = new Student(UUID.randomUUID(), "Elena", "Kolesova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group2A);
-        Student student8 = new Student(UUID.randomUUID(), "Olexandr", "Litvinov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group2A);
-        Student student9 = new Student(UUID.randomUUID(), "Sophia", "Minina", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group2A);
+        var student7 = new Student(UUID.randomUUID(), "Elena", "Kolesova", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group2A);
+        var student8 = new Student(UUID.randomUUID(), "Olexandr", "Litvinov", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group2A);
+        var student9 = new Student(UUID.randomUUID(), "Sophia", "Minina", OffsetDateTime.parse("2022-09-01T09:00:00+01:00"), group2A);
         List<Student> students2A = new ArrayList<>();
         students2A.add(student7);
         students2A.add(student8);
