68a6b32e312e330126636bd7e346bab25710ffc3
Tatiana Komarova <tatiana78977ua@gmail.com>
2022-12-26 20:32:08 +0200

    task-5:id was added in class Lesson

--- src/main/java/com/example/school/model/Lesson.java
+++ src/main/java/com/example/school/model/Lesson.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.UUID;
+
+import static java.util.UUID.randomUUID;
+
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 public class Lesson {
+    @Builder.Default
+    private UUID id = randomUUID();
     private Subject subject;
     private Teacher teacher;
     private Group group;
