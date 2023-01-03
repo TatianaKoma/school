67e6303b4a37c4ba26432905e8022589b357cdcc
Tatiana Komarova <tatiana78977ua@gmail.com>
2023-01-03 18:22:06 +0200

    task-7: FK were added to tables

--- src/main/resources/create_tables.sql
+++ src/main/resources/create_tables.sql
@@ -8,9 +8,10 @@ CREATE TABLE teachers
 
 CREATE TABLE "groups"
 (
-    id      UUID PRIMARY KEY NOT NULL,
-    name    TEXT             NOT NULL,
-    teacher teachers         NOT NULL
+    id         UUID PRIMARY KEY NOT NULL,
+    name       TEXT             NOT NULL,
+    teacher_id UUID             NOT NULL,
+    CONSTRAINT fk_teacher FOREIGN KEY (teacher_id) REFERENCES teachers (id)
 );
 
 CREATE TABLE subjects
@@ -25,24 +26,29 @@ CREATE TABLE students
     name      TEXT                     NOT NULL,
     surname   TEXT                     NOT NULL,
     startDate TIMESTAMP WITH TIME ZONE NOT NULL,
-    "group"   "groups"                 NOT NULL
+    group_id  UUID                     NOT NULL,
+    CONSTRAINT fk_group FOREIGN KEY (group_id) REFERENCES groups (id)
 );
 
 CREATE TABLE lessons
 (
-    id        UUID PRIMARY KEY         NOT NULL,
-    subject   subjects                 NOT NULL,
-    teacher   teachers                 NOT NULL,
-    "group"   "groups"                 NOT NULL,
-    topic     TEXT                     NOT NULL,
-    startDate TIMESTAMP WITH TIME ZONE NOT NULL
+    id         UUID PRIMARY KEY         NOT NULL,
+    subject    subjects                 NOT NULL,
+    teacher_id UUID                     NOT NULL,
+    group_id   UUID                     NOT NULL,
+    topic      TEXT                     NOT NULL,
+    startDate  TIMESTAMP WITH TIME ZONE NOT NULL,
+    CONSTRAINT fk_teacher FOREIGN KEY (teacher_id) REFERENCES teachers (id),
+    CONSTRAINT fk_group FOREIGN KEY (group_id) REFERENCES groups (id)
 );
 
 CREATE TABLE activities
 (
-    id        UUID PRIMARY KEY NOT NULL,
-    lesson    lessons          NOT NULL,
-    student   students         NOT NULL,
-    mark      INT,
-    isPresent boolean          NOT NULL
+    id         UUID PRIMARY KEY NOT NULL,
+    lesson_id  UUID             NOT NULL,
+    student_id UUID             NOT NULL,
+    mark       INT,
+    isPresent  boolean          NOT NULL,
+    CONSTRAINT fk_lesson FOREIGN KEY (lesson_id) REFERENCES lessons (id),
+    CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES students (id)
 );
