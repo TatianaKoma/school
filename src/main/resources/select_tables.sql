4cb2d170d39f99706b25df205b8c6c67f7742941
Tatiana Komarova <tatiana78977ua@gmail.com>
2023-01-17 14:26:27 +0200

    task-8:cte name was changed

--- src/main/resources/select_tables.sql
+++ src/main/resources/select_tables.sql
@@ -5,14 +5,14 @@ GROUP BY student_id
 ORDER BY truants DESC
 LIMIT 1;
 
-WITH cte AS (SELECT student_id,
-                    COUNT(ALL is_present) as truants,
-                    DENSE_RANK() OVER (
-                        ORDER BY COUNT(ALL is_present) DESC
-                        ) truants_rank
-             FROM activities
-             WHERE is_present=false
-             group by student_id)
+WITH cte_truant AS (SELECT student_id,
+                           COUNT(ALL is_present) as truants,
+                           DENSE_RANK() OVER (
+                               ORDER BY COUNT(ALL is_present) DESC
+                               )                    truants_rank
+                    FROM activities
+                    WHERE is_present = false
+                    GROUP BY student_id)
 SELECT student_id, truants
-FROM cte
+FROM cte_truant
 WHERE truants_rank = 1;
