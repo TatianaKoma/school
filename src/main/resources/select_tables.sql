c815057bfc4e23b397303e62721b0db37449aad4
Tatiana Komarova <tatiana78977ua@gmail.com>
2023-01-17 15:40:45 +0200

    task-8:table name was changed

--- src/main/resources/select_tables.sql
+++ src/main/resources/select_tables.sql
@@ -5,14 +5,14 @@ GROUP BY student_id
 ORDER BY truants DESC
 LIMIT 1;
 
-WITH cte_truant AS (SELECT student_id,
-                           COUNT(ALL is_present) as truants,
+WITH truants AS (SELECT student_id,
+                           COUNT(ALL is_present) as truancies,
                            DENSE_RANK() OVER (
                                ORDER BY COUNT(ALL is_present) DESC
-                               )                    truants_rank
+                               )                   truancies_rank
                     FROM activities
                     WHERE is_present = false
                     GROUP BY student_id)
-SELECT student_id, truants
-FROM cte_truant
-WHERE truants_rank = 1;
+SELECT student_id, truancies
+FROM truants
+WHERE truancies_rank = 1;
