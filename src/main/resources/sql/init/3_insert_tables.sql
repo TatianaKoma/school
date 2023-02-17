b9dbf8cd05dc42349c21a8b52c1487929c208ec4
Tatiana Komarova <tatiana78977ua@gmail.com>
2023-02-17 18:18:53 +0200

    task-14: name of files sql were changed

--- src/main/resources/sql/init/3_insert_tables.sql
+++ src/main/resources/sql/init/3_insert_tables.sql
@@ -128,22 +128,3 @@ VALUES ('9f10f9fc-9971-4e57-af45-2407fb793f4f', '281ba1dd-cda9-4c6e-846f-c67b30c
         '7c2938f7-43f2-462d-b59a-4d1ad34f7060', 9, true),
        ('8f26391f-2503-4f7f-9418-c835f2ab568b', '6aed2872-0756-40c8-8827-d835f666f3fe',
         '7c2938f7-43f2-462d-b59a-4d1ad34f7060', null, false);
-
-
-INSERT INTO activities (id, lesson_id, student_id, mark, is_present)
-VALUES ('8f26391f-2503-4f7f-9418-c835f2ab568b', '6aed2872-0756-40c8-8827-d835f666f3fe',
-        '7c2938f7-43f2-462d-b59a-4d1ad34f7060', 10, false);
-
-UPDATE activities
-SET mark= 10,
-    is_present= false
-WHERE id = '8f26391f-2503-4f7f-9418-c835f2ab568b';
-
-INSERT INTO activities (id, lesson_id, student_id, mark, is_present)
-VALUES ('9f10f9fc-9971-4e57-af45-2407fb793f4f', '281ba1dd-cda9-4c6e-846f-c67b30c20de1',
-        '7c2938f7-43f2-462d-b59a-4d1ad34f7060', 10, true);
-
-UPDATE activities
-SET lesson_id  ='281ba1dd-cda9-4c6e-846f-c67b30c20de1',
-    student_id = '7c2938f7-43f2-462d-b59a-4d1ad34f7060'
-WHERE id = '8f26391f-2503-4f7f-9418-c835f2ab568b';
